1e7876e5666ffdaf4646d5b0f4b65678da9c9349
Alexander Morozov <alexander.morozov@postindustria.com>
2016-12-01 10:07:23 +0200

    Added correct compare method for words

--- app/models/card.rb
+++ app/models/card.rb
@@ -7,8 +7,8 @@ class Card < ApplicationRecord
   before_save { self.review_date = Date.today + 3 }
 
   def original_text_should_not_be_eq_translated_text
-    if self.original_text.downcase.encode(Encoding::UTF_8).to_s ==
-       self.translated_text.downcase.encode(Encoding::UTF_8).to_s
+    if original_text.mb_chars.downcase.to_s.chars ==
+       translated_text.mb_chars.downcase.to_s.chars
       errors.add(:original_text,
                  "перевод не должен быть таким же как изначальный текст")
     end
