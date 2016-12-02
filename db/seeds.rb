a5c382ec79cb03ab3b6bf141200deb174fa6ff58
Alexander Morozov <alexander.morozov@postindustria.com>
2016-12-02 10:11:28 +0200

    Moved repeated code into the method

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,23 +9,20 @@
 require 'nokogiri'
 require 'open-uri'
 
-doc = Nokogiri::HTML(open('http://www.languagedaily.com/learn-german/vocabulary/common-german-words'))
-@words, @translations = [], []
-
-doc.xpath('//td[@class="bigLetter"]').each {|el| @words << el.text}
-doc.xpath('//td[preceding-sibling::td[@class="bigLetter"]][1]').each {|el| @translations << el.text}
-@words.length.times do |i|
-  Card.new(original_text: @words[i], translated_text: @translations[i]).save(validate: false)
-  print '.' if (i % 25).zero?
-end
-links = doc.xpath("//ul[preceding-sibling::h3[contains(.,'Index of most common German words')]]/li/a")
-links.each do |link|
-  @words2, @translations2 = [], []
-  doc2 = Nokogiri::HTML(open('http://www.languagedaily.com' + link['href']))
-  doc2.xpath('//td[@class="bigLetter"]').each {|el| @words2 << el.text}
-  doc2.xpath('//td[preceding-sibling::td[@class="bigLetter"]][1]').each {|el| @translations2 << el.text}
-  @words2.length.times do |i|
-    Card.new(original_text: @words2[i], translated_text: @translations2[i]).save(validate: false)
+def words_parsing(doc)
+  @words, @translations = [], []
+  doc.xpath('//td[@class="bigLetter"]').each {|el| @words << el.text}
+  doc.xpath('//td[preceding-sibling::td[@class="bigLetter"]][1]').each {|el| @translations << el.text}
+  @words.length.times do |i|
+    Card.new(original_text: @words[i], translated_text: @translations[i]).save(validate: false)
     print '.' if (i % 25).zero?
   end
+end
+
+doc = Nokogiri::HTML(open('http://www.languagedaily.com/learn-german/vocabulary/common-german-words'))
+words_parsing(doc)
+links = doc.xpath("//ul[preceding-sibling::h3[contains(.,'Index of most common German words')]]/li/a")
+links.each do |link|
+  doc2 = Nokogiri::HTML(open('http://www.languagedaily.com' + link['href']))
+  words_parsing(doc2)
 end
