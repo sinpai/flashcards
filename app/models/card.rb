697cf2ba7d114b3f68fd37bee523faee25b4d50d
Alexander Morozov <alexander.morozov@postindustria.com>
2016-12-04 23:49:47 +0200

    Fifth-task done

--- app/models/card.rb
+++ app/models/card.rb
@@ -7,7 +7,7 @@ class Card < ApplicationRecord
   before_save { self.review_date = Date.today + 3 }
   scope :on_review, ->(time) { where("review_date < ?", time) }
   scope :random_card, -> { order("RANDOM()").first }
-
+  
   def original_text_should_not_be_eq_translated_text
     if original_text.mb_chars.downcase.to_s.chars ==
        translated_text.mb_chars.downcase.to_s.chars
