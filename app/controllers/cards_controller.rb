71e72ab49991cf5835be2da8be0cd4e5d2d860d0
Alexander Morozov <alexander.morozov@postindustria.com>
2016-12-04 20:49:23 +0200

    Fifth task

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,12 +1,17 @@
 # coding: utf-8
 class CardsController < ApplicationController
 
+  helper_method :show_random_card, :check_card
+
   def index
     @cards = Card.order(:id)
   end
 
   def new
     @card = Card.new
+  end
+
+  def show
   end
 
   def edit
@@ -40,9 +45,23 @@ class CardsController < ApplicationController
     redirect_to cards_path, notice: "Карточка удалена успешно"
   end
 
+  def train
+    @card = Card.on_review(Date.today + 7).first
+    byebug
+  end
+
+  def check_card
+    if Card.find(params[:id]).original_text == params[:checktext]
+      Card.find(params[:id]).save
+      redirect_to cards_train_path, notice: 'Правильно!'
+    else
+      redirect_to cards_train_path, notice: 'Неправильно!'
+    end
+  end
+
   private
 
   def card_params
-    params.require(:card).permit(:original_text, :translated_text, :review_date)
+    params.require(:card).permit(:original_text, :translated_text, :review_date, :checktext)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'homepage#index'
 
   get "cards" => 'cards#index'
+  match 'cards/train' => 'cards#train', :via => :get
   resources :cards
   # controller :cards do
   #   get 'new' => :new
