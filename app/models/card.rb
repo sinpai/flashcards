14561d24b0fa6119e6f2bac0e53e4136612c3a43
Alexander Morozov <alexander.morozov@postindustria.com>
2016-12-04 20:49:23 +0200

    Fifth task

--- app/models/card.rb
+++ app/models/card.rb
@@ -5,6 +5,8 @@ class Card < ApplicationRecord
   validates :original_text, uniqueness: true
   validate :original_text_should_not_be_eq_translated_text
   before_save { self.review_date = Date.today + 3 }
+  scope :on_review, ->(time) { where("review_date < ?", time) }
+  scope :random_card, -> { order("RANDOM()").first }
 
   def original_text_should_not_be_eq_translated_text
     if original_text.mb_chars.downcase.to_s.chars ==
