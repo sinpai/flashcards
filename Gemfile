f87b436e13b1d33546882e734ac6ec9407295deb
Alexander Morozov <alexander.morozov@postindustria.com>
2016-12-08 23:39:55 +0200

    Using FactoryGirl and FFaker to simplify tests

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ group :development, :test do
   gem 'byebug', platform: :mri
   gem 'rspec-rails', "~> 3.1.0"
   gem 'factory_girl_rails', "~> 4.4.1"
+  gem 'ffaker'
 end
 
 group :development do
@@ -46,7 +47,6 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'byebug'
-  gem 'faker', '~> 1.4.3'
   gem 'capybara', '~> 2.4.3'
   gem 'database_cleaner', '~> 1.3.0'
   gem 'launchy', '~> 2.4.2'
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -3,54 +3,36 @@ require 'rails_helper'
 
 describe Card do
   it "is valid with original text, translated text and review date" do
-    card = Card.new(
-      original_text: 'Test',
-      translated_text: 'Тест',
-      review_date: '2016-12-12'
-    )
+    card = build(:card)
     expect(card).to be_valid
   end
   it "is invalid without original text" do
-    card = Card.new(
-      original_text: nil,
-      translated_text: 'test',
-      review_date: '2016-12-12'
-    )
+    card = build(:card, original_text: nil)
     card.valid?
     expect(card.errors[:original_text]).to include("can't be blank")
   end
   it "is invalid without translated text" do
-    card = Card.new(
-      original_text: 'rest',
-      translated_text: nil,
-      review_date: '2016-12-12'
-    )
+    card = build(:card, translated_text: nil)
     card.valid?
     expect(card.errors[:translated_text]).to include("can't be blank")
   end
   it "is valid with correct review date after save" do
-    card = Card.new(
-      original_text: 'Тест',
-      translated_text: 'test'
-    )
+    card = build(:card, review_date: nil)
     card.save
     card.valid?
     expect(card.review_date).to eq(Date.today + 3)
   end
   it "is invalid with dublicated original text" do
-    card = Card.new(
-      original_text: 'test',
-      translated_text: 'test'
-    )
+    card = build(:card, original_text: 'test', translated_text: 'test')
     card.valid?
     expect(card.errors[:original_text]).to include("перевод не должен быть таким же как изначальный текст")
   end
   it "prevents from saving card with the same original text" do
-    Card.create!(
+    create(:card,
       original_text: 'monkey',
       translated_text: 'обязьяна'
     )
-    card = Card.new(
+    card = build(:card,
       original_text: 'monkey',
       translated_text: 'манки'
     )
@@ -63,15 +45,12 @@ describe Card do
     expect(card1).not_to equal(card2)
   end
   it "should correctly check translation" do
-    card = Card.new(original_text: "house", translated_text: "Дом")
+    card = build(:card, original_text: "house", translated_text: "Дом")
     card.save
     expect(card.check_translation?("building")).to be false
   end
   it "should correctly update review date" do
-    card = Card.new(
-      original_text: 'Тест',
-      translated_text: 'test'
-    )
+    card = build(:card)
     card.save
     card.review_date = Date.today
     card.valid?
