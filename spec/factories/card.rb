ebe9b1a2a66e0b336780c62ad7adfdc95ef9fe25
Alexander Morozov <alexander.morozov@postindustria.com>
2016-12-13 22:49:57 +0200

    Fix in card factory

--- spec/factories/card.rb
+++ spec/factories/card.rb
@@ -5,6 +5,6 @@ FactoryGirl.define do
     original_text { FFaker::Food.ingredient }
     translated_text { FFaker::Food.fruit }
     review_date Date.today
-    user_id { rand(100).to_s }
+    user { rand(100).to_s }
   end
 end
