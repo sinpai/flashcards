9ec1a8442d1b35cd7e90304ab02ae01047ae02d1
Alexander Morozov <alexander.morozov@postindustria.com>
2016-12-21 13:02:29 +0200

    Fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,6 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    @packs = User.find(params[:id]).packs.all
   end
 
   # GET /users/new
--- app/models/pack.rb
+++ app/models/pack.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 class Pack < ApplicationRecord
   has_many :cards
   belongs_to :user
