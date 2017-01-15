1a2dd029ada8e19970a7ee973738b73ab0239c7d
sinpai <monaco90@gmail.com>
2017-01-15 14:12:08 +0200

    Migrations were deleted

--- db/migrate/20170113062858_add_iteration_to_cards.rb
+++
@@ -1,6 +0,0 @@
-class AddIterationToCards < ActiveRecord::Migration[5.0]
-  def change
-    change_column :cards, :interval, :float
-    add_column :cards, :iteration, :integer
-  end
-end
--- db/migrate/20170113092038_add_defaults_to_cards.rb
+++
@@ -1,6 +0,0 @@
-class AddDefaultsToCards < ActiveRecord::Migration[5.0]
-  def change
-    change_column :cards, :interval, :float, default: 1.0
-    change_column :cards, :iteration, :integer, default: 1
-  end
-end
