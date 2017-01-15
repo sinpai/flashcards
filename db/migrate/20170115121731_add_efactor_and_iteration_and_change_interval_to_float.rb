5b546a128d713da6c59c79d070d2ff413f0f675e
sinpai <monaco90@gmail.com>
2017-01-15 14:21:05 +0200

    Recreated migration

--- db/migrate/20170115121731_add_efactor_and_iteration_and_change_interval_to_float.rb
+++ db/migrate/20170115121731_add_efactor_and_iteration_and_change_interval_to_float.rb
@@ -1,4 +1,4 @@
-class AddEfactorAndIterationAndChangeIntervalInCards < ActiveRecord::Migration[5.0]
+class AddEfactorAndIterationAndChangeIntervalToFloat < ActiveRecord::Migration[5.0]
   def change
     add_column :cards, :efactor, :float, default: '2.5'
     change_column :cards, :interval, :float, default: 1.0
